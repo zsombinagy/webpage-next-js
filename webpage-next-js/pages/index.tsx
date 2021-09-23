5ca27c458b1ba40997a2590b3e75006e4f661b1d
iamzsombi <zsombinagy.14@gmail.com>
2021-09-23 22:59:27 +0200

    add more style

--- webpage-next-js/pages/index.tsx
+++ webpage-next-js/pages/index.tsx
@@ -8,23 +8,24 @@ const Home: NextPage = () => {
     <>
     <div className={styles.page}>
       <div className={styles.navbar}>
-        <img src="/logo.png" alt="logo" />
-        <div className={styles.button}>
-          <h1>Blog</h1>
-          <h1>Forums</h1>
-          <h1>Sign in</h1>
-        </div>
-
+          <img src="/logo.png" alt="logo" />
+          <div className={styles.button}>
+            <h1>Blog</h1>
+            <h1>Forums</h1>
+            <h1>Sign in</h1>
+          </div>
       </div>
-      <div className={styles.main_right}>
+      <div className={styles.main}>
+      <div className={styles.main_left}>
         <h1>Welcome <br /><span>Dark</span></h1>
         <button className={styles.log_in}>Log In</button>
 
 
       </div>
-      <div className={styles.main_left}>
+      <div className={styles.main_right}>
         <h1>To The <br />Side</h1>
         <button className={styles.sign_up}>Sign Up</button>
+      </div>
       </div>
       <p className={styles.text}>We work on amazing <span>projects for the world</span></p>
 
