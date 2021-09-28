9df8719f1f2ceeb3505416ebeb78dd98adf7039f
iamzsombi <zsombinagy.14@gmail.com>
2021-09-28 21:35:16 +0200

    blog html added, and some style

---
+++ webpage-next-js/pages/blog.tsx
@@ -0,0 +1,55 @@
+import type { NextPage } from 'next'
+import styles from '../styles/blog.module.sass'
+
+const blog: NextPage = () => {
+    return (
+        <>
+            <div className={styles.page}>
+                <div className={styles.navbar}>
+                    <img src="/logo.png" alt="logo" />
+                    <div className={styles.button}>
+                        <h1>Blog</h1>
+                        <h1>Forums</h1>
+                        <h1>Sign in</h1>
+                    </div>
+                </div>
+                <div className={styles.main}>
+                    <div className={styles.blog_title}>
+                        <h1>Blog Title #1</h1>
+                    </div>
+                    <div className={styles.author}>
+                        <img src="/profile.png" alt="profile" />
+                        <h4>@zsombornagy</h4>
+                    </div>
+                    <div className={styles.text}>
+                        <p className={styles.paragraph}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                        </p>
+                        <p className={styles.paragraph}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                        </p>
+                        <p className={styles.paragraph}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                        </p>
+                        <p className={styles.paragraph}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ipsa sequi architecto, fugiat quas id dignissimos iste illum mollitia voluptas consequuntur, excepturi quia facilis aliquid unde obcaecati! Dolorem, eveniet iste!
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default blog
