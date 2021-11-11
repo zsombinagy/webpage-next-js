634305b3dbb8eaa55d51d67c38e30ea5f469f6f8
iamzsombi <zsombinagy.14@gmail.com>
2021-11-11 18:55:38 +0100

    graphql almost ready, without body

--- webpage-next-js/pages/blog/[id].tsx
+++ webpage-next-js/pages/blog/[id].tsx
@@ -1,8 +1,41 @@
 import type { NextPage } from 'next'
 import styles from '../../styles/blog.module.sass'
 import Link from 'next/link'
+import { run } from 'ar-gql'
 
-const blog: NextPage = () => {
+const getTagsValue = (name: string, tags ) => {
+    return tags.find(tag => tag.name === name).value
+    
+  }
+
+export const getStaticProps = async ({params:{id}}) => {
+    const data = await run(
+`        query {
+            transaction(id: "${id}") {
+              id
+              tags {
+                name
+                value
+              }
+            }
+          }`
+    )
+
+
+    return  {
+        props: {blog: data.data.transaction}
+    }
+}
+
+export async function getStaticPaths() {
+
+    return {
+        paths: [], 
+        fallback: 'blocking'
+    }
+}
+
+const blog: NextPage = ({blog}) => {
 
 
 
@@ -13,10 +46,19 @@ const blog: NextPage = () => {
                     <img src="/logo.png" alt="logo" />
                     <div className={styles.button}>
                         <Link href="/"><a>Blog</a></Link>
-                        <Link href="/write"><a>write</a></Link>
+                        <Link href="/write"><a>Write</a></Link>
                         <Link href="/"><a>Sign in</a></Link>
                     
                     </div>
+                </div>
+                {console.log(blog)}
+                <div className={styles.main}>
+                    <div className={styles.blog_title}>{getTagsValue("Blog-name", blog.tags)}</div>
+                    <div className={styles.blog_author}>
+                        <img src="/profile.png" alt="profile" />
+                        <p>@{getTagsValue("Blog-username", blog.tags)}</p>
+                        
+                        </div>
                 </div>
             </div>
         </>
